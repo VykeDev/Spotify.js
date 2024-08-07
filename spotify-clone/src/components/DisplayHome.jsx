b0efc9cb4099f3012dae695b9a44d31a2fd99614
Rift <hegide.dev@gmail.com>
2024-08-07 03:32:05 -0400

    Display

---
+++ spotify-clone/src/components/DisplayHome.jsx
@@ -0,0 +1,42 @@
+import Navbar from "./Navbar";
+import { albumsData, songsData } from "../assets/frontend-assets/assets";
+import Albumitem from "./Albumitem";
+import Songitem from "./Songitem";
+
+function DisplayHome() {
+  return (
+    <>
+      <Navbar />
+      <div className="mb-4">
+        <h1 className="my-5 font-bold text-2xl">Featured Charts</h1>
+        <div className="flex overflow-auto">
+          {albumsData.map((item, index) => (
+            <Albumitem
+              key={index}
+              name={item.name}
+              desc={item.desc}
+              id={item.id}
+              image={item.image}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="mb-4">
+        <h1 className="my-5 font-bold text-2xl">Today's biggest hits</h1>
+        <div className="flex overflow-auto">
+          {songsData.map((item, index) => (
+            <Songitem
+              key={index}
+              name={item.name}
+              desc={item.desc}
+              id={item.id}
+              image={item.image}
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default DisplayHome;
--- spotify-clone/src/components/Player.jsx
+++ spotify-clone/src/components/Player.jsx
@@ -13,10 +13,10 @@ function Player() {
       <div className="flex flex-col items-center gap-1 m-auto">
         <div className="flex gap-4">
           <img className="w-4 cursor-pointer" src={assets.shuffle_icon} />
-          <img className="w-4 cursor-pointer" src={assets.prev_icon} />
-          <img className="w-4 cursor-pointer" src={assets.play_icon} />
-          <img className="w-4 cursor-pointer" src={assets.next_icon} />
-          <img className="w-4 cursor-pointer" src={assets.loop_icon} />
+          <img className="w-4 cursor-pointer" src={assets.prev_icon} alt="" />
+          <img className="w-4 cursor-pointer" src={assets.play_icon} alt="" />
+          <img className="w-4 cursor-pointer" src={assets.next_icon} alt="" />
+          <img className="w-4 cursor-pointer" src={assets.loop_icon} alt="" />
         </div>
         <div className="flex items-center gap-5">
           <p>1:06</p>
