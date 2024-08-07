aadbbe7bc5c651f643165b329e860cf1a702ae1f
Rift <hegide.dev@gmail.com>
2024-08-07 00:48:24 -0400

    Player

--- spotify-clone/src/App.jsx
+++ spotify-clone/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Sidebar from "./components/Sidebar";
+import Player from "./components/Player";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <div className="h-[90%] flex">
         <Sidebar />
       </div>
+      <Player />
     </div>
   );
 }
